18925a97095ac3059c22559f05f18d6764bdd1c5
ppPacific <hohohorlick259@gmail.com>
2026-07-22 21:17:05 +0800

    script update

---
+++ greenday.sh
@@ -0,0 +1,10 @@
+#This is bash scripting
+
+# Prompt the user for input
+echo "Please enter your name:"
+
+# Read the user's input
+read user_name
+
+# Print a customized greeting
+echo "Hello, $user_name! Welcome to Bash scripting."
