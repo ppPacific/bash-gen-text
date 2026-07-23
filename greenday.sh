493c88b7d97e5af673e87c42e53a6505dcb26c52
ppPacific <hohohorlick259@gmail.com>
2026-07-23 23:17:57 +0800

    add var to script

--- greenday.sh
+++ greenday.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mydog="Josh"
 # Prompt the user for input
 echo "Please enter your name:"
 
@@ -7,4 +8,4 @@ echo "Please enter your name:"
 read user_name
 
 # Print a customized greeting
-echo "Hello, $user_name! Welcome to Bash scripting."
+echo "Hello, $user_name! Welcome to Bash scripting. Can you see my dog over there? He is $mydog."
