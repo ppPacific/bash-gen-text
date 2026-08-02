48f0d2f87f89209e77c27a4f792c7073640de248
ppPacific <hohohorlick259@gmail.com>
2026-08-02 23:27:48 +0800

    update script

--- greenday.sh
+++ greenday.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mydog="Josh"
+now=$(date)
 # Prompt the user for input
 echo "Please enter your name:"
 
@@ -9,3 +10,4 @@ read user_name
 
 # Print a customized greeting
 echo "Hello, $user_name! Welcome to Bash scripting. Can you see my dog over there? He is $mydog."
+echo "Today is $now it has been 3 years already, I really miss him."
